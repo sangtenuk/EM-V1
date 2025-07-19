450113fec57dd72dd1592f00a5ced9e9bd707e3a
muhamadhadiahmad <muhamadhadi@gmail.com>
2025-07-20 06:44:38 +0800

    Updated AuthPage.tsx

--- src/components/auth/AuthPage.tsx
+++ src/components/auth/AuthPage.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+/* import React, { useState } from 'react' */
+ import { useState } from 'react' 
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../../lib/supabase'
 import toast from 'react-hot-toast'
