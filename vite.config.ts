1d30f4b933d7bb809b39eec9e1064065ad9c949d
muhamadhadiahmad <muhamadhadi@gmail.com>
2025-07-20 07:38:39 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 🔥 Import path untuk alias
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // ✅ Tambah alias @ ke folder src
+    },
+  },
   server: {
     port: 5173,
-    host: true
-  }
+    host: true,
+  },
 })
