06474e5810f02469c7e2f4fb78319688b3fb0334
sangtenuk <89859661+sangtenuk@users.noreply.github.com>
2025-07-29 16:01:59 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path' // ✅ Penting untuk alias
 
 export default defineConfig({
+  base: '/', // jangan guna './'
   plugins: [react()],
   resolve: {
     alias: {
